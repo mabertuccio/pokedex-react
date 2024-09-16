29067aef31317daebb2b5a7d1a81b40f58da8d6f
matutuam <tutebertuccio@hotmail.com>
2024-09-16 11:26:25 -0300

    feat: create a simple footer to the structure

--- src/Pokedex.jsx
+++ src/Pokedex.jsx
@@ -1,7 +1,14 @@
+import React from "react";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function Pokedex() {
-  return <Header />;
+  return (
+    <React.Fragment>
+      <Header />
+      <Footer />
+    </React.Fragment>
+  );
 }
 
 export default Pokedex;
---
+++ src/components/small/Links.jsx
@@ -0,0 +1,33 @@
+import PropTypes from "prop-types";
+import LinkedInLogo from "./LinkedInLogo";
+import GitHubLogo from "./GitHubLogo";
+
+const Link = ({ socialURL, logoSVG }) => {
+  return (
+    <a className=" mr-3" href={socialURL} target="_blank">
+      {logoSVG}
+    </a>
+  );
+};
+
+Link.propTypes = {
+  socialURL: PropTypes.string,
+  logoSVG: PropTypes.element,
+};
+
+const Links = () => {
+  return (
+    <div className="flex flex-row mt-4">
+      <Link
+        socialURL="https://www.linkedin.com/in/matiasbertuccio"
+        logoSVG={LinkedInLogo}
+      />
+      <Link
+        socialURL="https://www.github.com/mabertuccio"
+        logoSVG={GitHubLogo}
+      />
+    </div>
+  );
+};
+
+export default Links;
