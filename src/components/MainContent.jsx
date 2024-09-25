3bb27f2ea5089dd94b08158076049432f6531df9
matutuam <tutebertuccio@hotmail.com>
2024-09-25 10:47:41 -0300

    chore: comment out search bar component

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,10 +1,9 @@
-import SearchBar from "./small/SearchBar";
+//import SearchBar from "./small/SearchBar";
 import Panels from "./small/Panels";
 
 const MainContent = () => {
   return (
     <div className="mx-auto mt-8">
-      <SearchBar />
       <Panels />
     </div>
   );
